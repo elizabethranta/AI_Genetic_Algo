a4ed6b66b135510aeb305b798957cb9f792be842
Andrew Motz <andrewmozza555@gmail.com>
2021-11-14 22:33:07 -0800

    Created basic leg scripts

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walker : MonoBehaviour
+{
+    public LegController leftLeg;
+    public LegController rightLeg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
